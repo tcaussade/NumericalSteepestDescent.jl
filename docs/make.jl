734840e36efdde16b32f3e7782e7e94a91028580
Thomas Caussade <tcaussade@Thomass-MacBook-Air.local>
2026-06-04 14:58:56 +0100

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,21 @@ using Documenter, NumericalSteepestDescent
 
 makedocs(;
     modules = [NumericalSteepestDescent],
-    format = Documenter.HTML(
-        canonical = "https://tcaussade.github.io/NumericalSteepestDescent.jl/",
-        repolink = "https://github.com/tcaussade/NumericalSteepestDescent.jl"
-    ),
-    pages = [
-        "Home" => "index.md",
-        # "Gaussian Quadrature" => "gaussquadrature.md",
-        # "Benchmark" => "benchmark.md",
-        # "Roots of Bessel function" => "besselroots.md",
-        # "Misc." => "misc.md",
-        # "References" => "reference.md",
-    ],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    # format = Documenter.HTML(
+    #     canonical = "https://tcaussade.github.io/NumericalSteepestDescent.jl/",
+    #     repolink = "https://github.com/tcaussade/NumericalSteepestDescent.jl"
+    # ),
+    
     repo = "https://github.com/tcaussade/NumericalSteepestDescent.jl/blob/{commit}{path}#L{line}",
     sitename = "NumericalSteepestDescent.jl",
     authors = "Thomas Caussade",
+    pages = [
+        "Home" => "index.md",
+    ],
 )
 
-deploydocs(; repo = "github.com/tcaussade/NumericalSteepestDescent.jl.git")
+deploydocs(
+    repo   = "github.com/tcaussade/NumericalSteepestDescent.jl.git",
+    target = "build",
+)
