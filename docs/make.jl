06363460d4090b16de71fb98ce23ea40081ac8c6
Thomas Caussade <tcaussade@Thomass-MacBook-Air.local>
2026-06-04 13:06:24 +0100

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,21 @@
 using Documenter, NumericalSteepestDescent
 
-makedocs(sitename="NumericalSteepestDescent.jl")
+makedocs(;
+    modules = [NumericalSteepestDescent],
+    format = Documenter.HTML(
+        repolink = "https://github.com/tcaussade/NumericalSteepestDescent.jl"
+    ),
+    pages = [
+        "Home" => "index.md",
+        # "Gaussian Quadrature" => "gaussquadrature.md",
+        # "Benchmark" => "benchmark.md",
+        # "Roots of Bessel function" => "besselroots.md",
+        # "Misc." => "misc.md",
+        # "References" => "reference.md",
+    ],
+    repo = "https://github.com/tcaussade/NumericalSteepestDescent.jl",
+    sitename = "NumericalSteepestDescent.jl",
+    authors = "Thomas Caussade",
+)
+
+deploydocs(; repo = "github.com/tcaussade/NumericalSteepestDescent.jl")
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,19 +1,11 @@
 # NumericalSteepestDescent.jl Documentation
 
-
-```@contents
-```
-
 ## Functions
 
 ```@docs
 nsd
 ```
 
-## Index
-
-```@index
-```
 
 
 
